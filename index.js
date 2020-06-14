87682ad3c6f069f4cdfdf42bd88298d13543099f
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-06-14 23:09:54 +0800

    discard files

--- index.js
+++ index.js
@@ -4,28 +4,35 @@ const { type } = require("os")
 const port = process.env.PORT || 80
 
 const app = express()
+var record = "";
 
 app.get("/", (req, res) => {
     console.log(`${JSON.stringify(req.query)}`)
-    fs.appendFile('./out.txt', `${JSON.stringify(req.query)}\n`, (err) => {
-		if (err) throw err
-    })
+    // fs.appendFile('./out.txt', `${JSON.stringify(req.query)}\n`, (err) => {
+	// 	if (err) throw err
+    // })
+
+    record = record + `${JSON.stringify(req.query)}\n`
     
-    res.status(200)
+    // res.status(200)
     res.send("Ok")
 })
 
 app.get("/logs", (req, res) => {
-    fs.readFile("./out.txt", (err, data) => {
-        if(err){
-            res.send("Error");
-        }else{
-            let str = data.toString()
-            console.log(typeof(str))
-            console.log(str)
-            res.send(str)
-        }
-    })
+    // fs.readFile("./out.txt", (err, data) => {
+    //     if(err){
+    //         res.send("Error");
+    //     }else{
+    //         let str = data.toString()
+    //         console.log(typeof(str))
+    //         console.log(str)
+    //         res.send(str)
+    //     }
+    // })
+    let str = record.toString()
+    // console.log(typeof(str))
+    // console.log(str)
+    res.send(str)
 })
 
 app.listen(port, () => {
