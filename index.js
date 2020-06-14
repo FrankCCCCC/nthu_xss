371b811bf133e72127c54d20cd77ba34e903cbb3
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-06-15 00:11:41 +0800

    msg_board done

--- index.js
+++ index.js
@@ -12,30 +12,13 @@ app.use(timeout("5s"))
 
 app.get("/", (req, res) => {
     console.log(`${JSON.stringify(req.query)}`)
-    // fs.appendFile('./out.txt', `${JSON.stringify(req.query)}\n`, (err) => {
-	// 	if (err) throw err
-    // })
 
     record = record + `${JSON.stringify(req.query)}\n`
-    
-    // res.status(200)
     res.send(Math.random().toString())
 })
 
 app.get("/logs", (req, res) => {
-    // fs.readFile("./out.txt", (err, data) => {
-    //     if(err){
-    //         res.send("Error");
-    //     }else{
-    //         let str = data.toString()
-    //         console.log(typeof(str))
-    //         console.log(str)
-    //         res.send(str)
-    //     }
-    // })
     let str = record.toString()
-    // console.log(typeof(str))
-    // console.log(str)
     res.send(str)
 })
 
