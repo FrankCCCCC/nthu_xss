c63113953e1d3c2b5aa85468c410b413202733c8
FrankCCCCC <unaxultraspaceos5@gmail.com>
2020-06-14 22:33:09 +0800

    setup server

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express")
 const fs = require("fs")
+const { type } = require("os")
 const port = process.env.PORT || 80
 
 const app = express()
 
 app.get("/", (req, res) => {
-    fs.writeFile('./out.txt', req.params, (err) => {
+    console.log(`${JSON.stringify(req.query)}`)
+    fs.appendFile('./out.txt', `${JSON.stringify(req.query)}\n`, (err) => {
 		if (err) throw err
     })
     
     res.status(200)
+    res.send("Ok")
+})
+
+app.get("/logs", (req, res) => {
+    fs.readFile("./out.txt", (err, data) => {
+        if(err){
+            res.send("Error");
+        }else{
+            let str = data.toString()
+            console.log(typeof(str))
+            console.log(str)
+            res.send(str)
+        }
+    })
 })
 
 app.listen(port, () => {
